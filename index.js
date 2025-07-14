67a4d21eb8be3b28aa296615668b1d9259bfb5af
HeyImZoid <125305360+HeyImZoid@users.noreply.github.com>
2025-07-14 22:38:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
+const keep_alive = require('./keep_alive.js')
 
 class DiscordClient {
 client = new Client({
